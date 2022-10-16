4e19cbc3dbc9f01789c44f3b8cc02c9f1f8d5a08
GameMutPlay14 <77333367+Savin1play140@users.noreply.github.com>
2022-10-16 05:57:21 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,23 +1,50 @@
-function button() {
-	qary = confirm("Перейти на \"WebOS\"? (только на английском языке!)")
-	if (qary == true) {
-		alert("ok")
-		window.location.href = "/webos.php"
-	} else {
-		alert("ok")
-	}
+var ad_block;
+var ad_text = [
+	"gmp14", //1
+	"gmp14 top", //2
+	"gmp", //3
+	"gmp top", //4
+	"author top", //5
+	"kyb co.", //6
+	"gmp co.", //7
+	"ByteLite co.", //8
+	"ByteLite lib.", //9
+	"YouTube gmp", //10
+	"YouTube kyb", //11
+	"kub and gmp", //12
+	"gmp and kub", //13
+	"top 1, top 2", //14
+	"server ByteLite", //15
+	"server ByteLite Minecraft", //16
+	"1140", //17
+	"1440", //18
+	"kyb", //19
+	"kyb top", //20
+	"kybomaster", //21
+	"kybomaster top", //22
+	"fact: this site is mostly OOP", //23
+	" Minecraft server: bytelite.ru:19132", //24
+];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
-function getText(lang) {
-	text_eng = "this page in ENGLISH"
-	text_rus = "эта страница на Русском"
-	text_ukr = "цю сторінку в Україні"
-	if (lang == "EN") {
-		return text_eng
+function connectAd() {
+	ad_block = document.getElementsByTagName('ad');
+	for (let i=0;i<ad_block.length;i++){
+		rand = getRandomInt(24);
+		ad_block[i].innerHTML = ad_text[rand];
 	}
-	if (lang == "RU") {
-		return text_rus
+	return true;
+}
+function clearAd() {
+	ad_block = document.getElementsByTagName('ad');
+	for (let i=0;i<ad_block.length;i++){
+		ad_block[i].innerText = "";
 	}
-	if (lang == "UK") {
-		return text_ukr
-	}
+	return true;
+}
+window.onload = function()
+{
+	connectAd();
+	console.log("enabled ad");
 }
